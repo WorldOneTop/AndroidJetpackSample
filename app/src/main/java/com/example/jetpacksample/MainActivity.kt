09815f5e4484f8e16fd07666bf8f3d3aa6115aa3
dlwpdlf147 <dlwpdlf147@naver.com>
2022-07-14 14:53:18 +0900

    네비게이션 #2, drawer navigation layout setting

--- app/src/main/java/com/example/jetpacksample/MainActivity.kt
+++ app/src/main/java/com/example/jetpacksample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.jetpacksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,13 +13,38 @@ import com.example.jetpacksample.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         val navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
                 as NavHostFragment).navController
         binding.navBottom.setupWithNavController(navController)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+
+        binding.navDrawer.setupWithNavController(navController)
+
+
     }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when(item.itemId){
+                android.R.id.home -> binding.mainActivityLayout.openDrawer(Gravity.START)
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
+        override fun onBackPressed() {
+            if(binding.mainActivityLayout.isDrawerOpen(binding.navDrawer))
+                binding.mainActivityLayout.closeDrawer(Gravity.START)
+            else
+                super.onBackPressed()
+        }
 }
